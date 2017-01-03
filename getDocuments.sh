c976ac06ef1990d04f1cc9af11438115817613c1
Burke Chih-Jen Ko <burkeker@gmail.com>
2017-01-03 21:28:51 +0100

    Updated getDocuments script.

--- getDocuments.sh
+++ getDocuments.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+LOGFILE=/var/log/mendeley/documentRetrieval.log
+touch $LOGFILE
+cat /dev/null > $LOGFILE
+
+exec 2> >(perl -pe '$x=`date "+%d %b %Y %H:%M %p"`;chomp($x);$_=$x." ".$_' >/var/log/mendeley/documentRetrieval.log)
+
+STARTTIME=$(date +%s)
 node app.js &
 casperjs authenticate.js
+
+ENDTIME=$(date +%s)
+echo "It takes $(($ENDTIME - $STARTTIME)) seconds to complete Mendeley document retrieval."
+(>&2 echo "It takes $(($ENDTIME - $STARTTIME)) seconds to complete Mendeley document retrieval.")
