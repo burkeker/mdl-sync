86eebd5fcf6d7022102699fafd17e999aff85e1c
Burke Chih-Jen Ko <burkeker@gmail.com>
2017-01-03 22:38:53 +0100

    Authenticate.js should honour the appConfig settins.

--- getDocuments.sh
+++ getDocuments.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+exec 2> >(perl -pe '$x=`date "+%d %b %Y %H:%M %p"`;chomp($x);$_=$x." ".$_' >/var/log/mendeley/documentRetrieval.log)
 
 LOGFILE=/var/log/mendeley/documentRetrieval.log
 touch $LOGFILE
 cat /dev/null > $LOGFILE
 
-exec 2> >(perl -pe '$x=`date "+%d %b %Y %H:%M %p"`;chomp($x);$_=$x." ".$_' >/var/log/mendeley/documentRetrieval.log)
-
 STARTTIME=$(date +%s)
-node app.js >> $LOGFILE 2>&1 &
+node app.js > $LOGFILE 2>&1 &
 casperjs authenticate.js >> $LOGFILE 2>&1
 
 ENDTIME=$(date +%s)
