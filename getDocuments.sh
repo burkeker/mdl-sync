6b355a145597a9e9b001ccbbdfaa8d68fad7ecb3
Burke Chih-Jen Ko <burkeker@gmail.com>
2017-01-03 22:16:10 +0100

    Updated log output.

--- getDocuments.sh
+++ getDocuments.sh
@@ -7,8 +7,8 @@ cat /dev/null > $LOGFILE
 exec 2> >(perl -pe '$x=`date "+%d %b %Y %H:%M %p"`;chomp($x);$_=$x." ".$_' >/var/log/mendeley/documentRetrieval.log)
 
 STARTTIME=$(date +%s)
-node app.js &
-casperjs authenticate.js
+node app.js >> $LOGFILE 2>&1 &
+casperjs authenticate.js >> $LOGFILE 2>&1
 
 ENDTIME=$(date +%s)
 echo "It takes $(($ENDTIME - $STARTTIME)) seconds to complete Mendeley document retrieval."
